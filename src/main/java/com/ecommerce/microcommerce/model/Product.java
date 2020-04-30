73d36950f20fb6062bdb8a10a4b5ee18c2379624
florian-0804 <florian.leclercq80@gmail.com>
2020-04-30 14:21:59 +0200

    First commit

---
+++ src/main/java/com/ecommerce/microcommerce/model/Product.java
@@ -0,0 +1,69 @@
+package com.ecommerce.microcommerce.model;
+
+import com.fasterxml.jackson.annotation.JsonFilter;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import org.hibernate.validator.constraints.Length;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.validation.constraints.Min;
+
+@Entity
+//@JsonFilter("monFiltreDynamique")
+public class Product {
+
+    @Id
+    @GeneratedValue
+    private int id;
+
+    @Length(min = 3, max = 20, message = "Nom trop long ou trop court. Et oui mes messages sont plus stylés que ceux de Spring")
+    private String nom;
+    @Min(value = 1)
+    private int prix;
+    private int prixAchat;
+
+    public Product() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public int getPrix() {
+        return prix;
+    }
+
+    public void setPrix(int prix) {
+        this.prix = prix;
+    }
+
+    public int getPrixAchat() {
+        return prixAchat;
+    }
+
+    public void setPrixAchat(int prixAchat) {
+        this.prixAchat = prixAchat;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{"+
+                "id=" + id +
+                ", nom='"+ nom + '\'' +
+                ", prix=" + prix+ '}';
+    }
+}
